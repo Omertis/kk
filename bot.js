ba74ded65253cd1f7cb4d75b3702d733993658cd
Omertis <36013948+Omertis@users.noreply.github.com>
2018-04-24 18:04:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ member => {
 
  
 
-                    let modlog2 = client.channels.find('name', 'jelly-chat');
+                    let modlog2 = client.channels.find('name', 'chat');
 
 
  
@@ -52,7 +52,50 @@ member => {
     
     
     
+client.on("guildMemberAdd",
 
+member => {
+
+    var moment = require("moment");
+
+ 
+
+                    let modlog2 = client.channels.find('name', 'welcome');
+
+
+ 
+
+         moment.locale('ar-ly');
+
+         var h = member.user;
+
+        let heroo = new Discord.RichEmbed()
+
+        .setColor('#6fc167')
+
+        .setThumbnail(h.avatarURL)
+
+        .setAuthor(h.username,h.avatarURL)
+
+        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
+
+         .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
+
+     modlog2.send({embed:heroo});
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+});
 
 
 
@@ -426,7 +469,78 @@ client.channels.get("419936453343313922").sendFile(canvas.toBuffer())
 
 }
 });
+client.on('guildMemberAdd', member => {
 
+     if (member.guild.id === "436839106660663307") {
+    
+if (member.user.bot) return;
+var Canvas = require('canvas')
+var jimp = require('jimp')
+const w = ['./img/jelly.png'];
+        let Image = Canvas.Image,
+            canvas = new Canvas(749, 198),
+            ctx = canvas.getContext('2d');
+        ctx.patternQuality = 'bilinear';
+        ctx.filter = 'bilinear';
+        ctx.antialias = 'subpixel';
+        ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
+        ctx.shadowOffsetY = 2;
+        ctx.shadowBlur = 2;
+        ctx.stroke();
+        ctx.beginPath();
+
+        fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+            if (err) return console.log(err);
+            let BG = Canvas.Image;
+            let ground = new Image;
+            ground.src = Background;
+            ctx.drawImage(ground, 0, 0, 749, 198);
+
+})
+
+                let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
+                jimp.read(url, (err, ava) => {
+                    if (err) return console.log(err);
+                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                        if (err) return console.log(err);
+                        ctx.font = '35px agent_orange';
+                        ctx.fontSize = '40px';
+                        ctx.fillStyle = "#7FF5DA";
+                        ctx.textAlign = "center";
+                        ctx.fillText(" Welcome to " + member.guild.name , 300, 55);
+
+                        //ur name
+                        ctx.font = '40px Impact';
+                        ctx.fontSize = '48px';
+                        ctx.fillStyle = "#7FF586";
+                        ctx.textAlign = "center";
+                        ctx.fillText(member.user.username, 300, 110);
+
+                         ctx.font = '30px Impact';
+                        ctx.fontSize = '20px';
+                        ctx.fillStyle = "#7FCDF5";
+                        ctx.textAlign = "center";
+                        ctx.fillText("Member Number" + member.guild.memberCount, 300, 150);
+
+
+                        //Avatar
+                    let Avatar = Canvas.Image;
+                              let ava = new Avatar;
+                              ava.src = buf;
+                              ctx.beginPath();
+                              ctx.arc(75, 101, 63, 0, Math.PI*2);
+                                 ctx.closePath();
+                                 ctx.clip();
+                                 ctx.drawImage(ava, 10, 38, 128, 126);     
+client.channels.get("436866763074961409").sendFile(canvas.toBuffer())
+
+
+
+})
+})
+
+}
+});
 var dat = JSON.parse("{}");
 function forEachObject(obj, func) {
     Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
@@ -469,6 +583,46 @@ client.on("guildMemberAdd", (member) => {
     })
 });
 
-
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "TJ")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', 'welcome');
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "TJ")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+ channel.send(` **Invited by**  ${Invite.inviter} `)            
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
+});
 client.login(process.env.BOT_TOKEN);
 
